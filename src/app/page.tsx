22bf08151d948b147b44465f593cf3e7d8299426
GregoryLebret-STW <gregory.lebret@severntrent.co.uk>
2024-03-10 13:42:58 +0000

    fix heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import { Heading } from '@/components/heading';
 const Home = () => {
   return (
     <main className="container mx-auto py-4 flex justify-center items-center flex-col">
-      <Heading>Home</Heading>
+      <Heading level="h1">Home</Heading>
       <Heading level="h2">Home</Heading>
+      <Heading level="h3">Home</Heading>
+      <Heading level="h4">Home</Heading>
+      <Heading level="h5">Home</Heading>
+      <Heading level="h6">Home</Heading>
       <Button>Button</Button>
     </main>
   );
--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,32 +1,36 @@
 import { Slot } from '@radix-ui/react-slot';
-import { cva, type VariantProps } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 import { ComponentPropsWithRef, forwardRef } from 'react';
 
 const headingVariants = cva(' font-bold dark:text-white', {
   variants: {
-    as: {
-      default: 'text-5xl',
+    level: {
+      h1: 'text-5xl',
       h2: 'text-4xl',
+      h3: 'text-3xl',
+      h4: 'text-2xl',
+      h5: 'text-xl',
+      h6: 'text-lg',
     },
   },
   defaultVariants: {
-    as: 'default',
+    level: 'h1',
   },
 });
 
-export interface HeadingProps
-  extends ComponentPropsWithRef<'h1'>,
-    VariantProps<typeof headingVariants> {
-  level?: string;
+type Level = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+export interface HeadingProps extends ComponentPropsWithRef<'h1'> {
+  level: Level;
   asChild?: boolean;
 }
 
 const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
-  ({ className, level = 'h1', asChild = false, as, ...props }, ref) => {
+  ({ className, level, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : level;
-    return <Comp className={cn(headingVariants({ as }))} ref={ref} {...props} />;
+    return <Comp className={cn(headingVariants({ level, className }))} ref={ref} {...props} />;
   }
 );
 Heading.displayName = 'Heading';
