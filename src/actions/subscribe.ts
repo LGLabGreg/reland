22a600c2ba52495aea1270b80e47645ee6d6fa74
GregoryLebret-STW <gregory.lebret@severntrent.co.uk>
2024-03-23 11:55:29 +0000

    newsletter

---
+++ src/actions/subscribe.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import mailchimp from '@mailchimp/mailchimp_marketing';
+import { newsletterEmailSchema } from '@/lib/zod';
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_SERVER_PREFIX,
+});
+
+type SubscribeResponse = {
+  success: boolean;
+};
+
+export async function subscribe(email: string): Promise<SubscribeResponse> {
+  console.log('subscribe', email);
+  try {
+    newsletterEmailSchema.parse({ email });
+    await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID!, {
+      email_address: email,
+      status: 'subscribed',
+    });
+    return { success: true };
+  } catch (error) {
+    return { success: false };
+  }
+}
---
+++ src/components/newsletter-subscribe.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+import { subscribe } from '@/actions/subscribe';
+import { newsletterEmailSchema } from '@/lib/zod';
+
+const NewsletterSubscribe = () => {
+  const [message, setMessage] = useState<string | null>(null);
+  const form = useForm<z.infer<typeof newsletterEmailSchema>>({
+    resolver: zodResolver(newsletterEmailSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof newsletterEmailSchema>) => {
+    setMessage(null);
+    const response = await subscribe(values.email);
+    if (response.success) {
+      setMessage('Thanks for subscribing');
+    } else {
+      setMessage('There was an error, please try again');
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8" noValidate>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Subscribe to get latest offers</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage>{message}</FormMessage>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default NewsletterSubscribe;
